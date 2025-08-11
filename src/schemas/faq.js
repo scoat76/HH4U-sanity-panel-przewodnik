136eb3c900b953c904f44244e75562eb22b3762f
scoat76 <33258885+scoat76@users.noreply.github.com>
2025-08-11 22:20:43 +0200

    Create faq.js

---
+++ src/schemas/faq.js
@@ -0,0 +1,10 @@
+// schemas/faq.js
+export default {
+  name: 'faq',
+  title: 'Pytania i Odpowiedzi (FAQ)',
+  type: 'document',
+  fields: [
+    { name: 'question', title: 'Pytanie', type: 'string' },
+    { name: 'answer', title: 'Odpowiedź', type: 'text' },
+  ],
+}
